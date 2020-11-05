f2b533a65fe0987a8e4b69a294f51b7d35e91584
Your Name <you@example.com>
2020-11-05 13:07:57 +0530

    first commit - only addition and subtraction programs

---
+++ Addition.c
@@ -0,0 +1,9 @@
+void main()
+{
+	int a,b,c;
+	a = 10;
+	b = 20;
+	c = a + b;
+	
+	printf("Sum of a and b is %d", c);
+}
---
+++ Subtraction.c
@@ -0,0 +1,9 @@
+void main()
+{
+	int a,b,c;
+	a = 10;
+	b = 20;
+	c = a - b;
+	
+	printf("Diff of a and b is %d", c);
+}
